82ec91334d3690bda3f8eacc23d58dc5e68b93f3
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-11-02 14:22:50 -0500

    latest commit

--- SQL FIles/questionone.sql
+++ SQL FIles/questionone.sql
@@ -2,5 +2,5 @@
 employee number, last name, first name, gender, and salary.*/
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees e
-JOIN salaries s
+LEFT JOIN salaries s
 ON e.emp_no = s.emp_no;
