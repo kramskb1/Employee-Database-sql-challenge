22ca38ddbba2690c83f4a49c5073c41b973517ed
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-11-06 14:38:26 -0500

    last commit

---
+++ SQL FIles/questionthree.sql
@@ -0,0 +1,6 @@
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_managers as dm
+INNER JOIN departments AS d
+	ON dm.dept_no=d.dept_no
+LEFT JOIN employees AS e
+	ON dm.emp_no=e.emp_no;
