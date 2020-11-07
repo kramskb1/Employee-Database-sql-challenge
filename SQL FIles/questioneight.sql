c9146c720a82d1c4d11d7169553a3c876dddd8a5
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-11-07 16:00:19 -0500

    last commit

--- SQL FIles/questioneight.sql
+++ SQL FIles/questioneight.sql
@@ -1,4 +1,4 @@
-/*n descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
+/*In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
 SELECT last_name, COUNT(*) AS freq_count
 FROM employees
 GROUP BY last_name
