36643bdc8d4f1a990a9e8b189a18a2160e1f1db6
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-11-07 15:03:07 -0500

    last commit

---
+++ SQL FIles/questionseven.sql
@@ -0,0 +1,11 @@
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
